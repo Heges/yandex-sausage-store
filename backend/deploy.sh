546533dbb5e516489eb8a0872bbdec537dfdca01
gaag_alexander <ikunkka92@gmail.com>
2024-07-07 16:52:23 +0300

    fix

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,9 +2,9 @@
 set -xe
 
 sudo cp -rf backend.service /etc/systemd/system/backend.service
-sudo rm -rf /home/jarservice/sausage-store.jar
+sudo rm -rf /home/student/sausage-store.jar
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/repository/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
-sudo cp -rf ./sausage-store.jar /var/jarservice/sausage-store.jar
+sudo cp -rf ./sausage-store.jar /var/student/sausage-store.jar
 
 sudo systemctl daemon-reload
 sudo systemctl enable backend.service
