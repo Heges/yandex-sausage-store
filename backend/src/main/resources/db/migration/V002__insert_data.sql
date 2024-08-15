a30fcf6758c70c52e74449a697f78e94e9e7b731
gaag_alexander <ikunkka92@gmail.com>
2024-08-15 20:03:42 +0300

    Update V002__insert_data.sql

--- backend/src/main/resources/db/migration/V002__insert_data.sql
+++ backend/src/main/resources/db/migration/V002__insert_data.sql
@@ -4,7 +4,7 @@ INSERT INTO product (id, name, picture_url, price)VALUES(3, 'Молочная', 
 INSERT INTO product (id, name, picture_url, price)VALUES(4, 'Нюренбергская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/3.jpg', 315.00);
 INSERT INTO product (id, name, picture_url, price)VALUES(5, 'Мюнхенская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg', 330.00);
 INSERT INTO product (id, name, picture_url, price)VALUES(6, 'Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg', 189.00);
-INSERT INTO order_product (quantity, order_id, product_id);
+INSERT INTO order_product (quantity, order_id, product_id) SELECT floor(1+random()*50)::int, i, 1 + floor(random()*6)::int % 6 FROM generate_series(1, 10000000) s(i);
 INSERT INTO orders (id, status, date_created)
 SELECT
     i,(ARRAY['pending', 'shipped', 'cancelled'])[FLOOR(RANDOM() * 3 + 1)],DATE(NOW() - (RANDOM() * (NOW() + '90 days' - NOW())))FROM generate_series(1, 10000000) s(i);
