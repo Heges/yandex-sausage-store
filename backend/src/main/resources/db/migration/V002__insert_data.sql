cfd01230b82f245b8cd60a299b9627ed69b7c73c
gaag_alexander <ikunkka92@gmail.com>
2024-08-15 19:50:30 +0300

    Update V002__insert_data.sql

--- backend/src/main/resources/db/migration/V002__insert_data.sql
+++ backend/src/main/resources/db/migration/V002__insert_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO product (id, name, picture_url, price)VALUES(1, 'Сливочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/6.jpg', 320.00);
-INSERT INTO product (id, name, picture_url, price)VALUES(2, 'Особая', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg', 179.00),;
+INSERT INTO product (id, name, picture_url, price)VALUES(2, 'Особая', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg', 179.00);
 INSERT INTO product (id, name, picture_url, price)VALUES(3, 'Молочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/4.jpg', 225.00);
 INSERT INTO product (id, name, picture_url, price)VALUES(4, 'Нюренбергская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/3.jpg', 315.00);
 INSERT INTO product (id, name, picture_url, price)VALUES(5, 'Мюнхенская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg', 330.00);
@@ -7,8 +7,5 @@ INSERT INTO product (id, name, picture_url, price)VALUES(6, 'Русская', 'h
 INSERT INTO order_product (quantity, order_id, product_id);
 INSERT INTO orders (id, status, date_created)
 SELECT
-    i,
-    (ARRAY['pending', 'shipped', 'cancelled'])[FLOOR(RANDOM() * 3 + 1)],
-    DATE(NOW() - (RANDOM() * (NOW() + '90 days' - NOW())))
-FROM generate_series(1, 10000000) s(i);
+    i,(ARRAY['pending', 'shipped', 'cancelled'])[FLOOR(RANDOM() * 3 + 1)],DATE(NOW() - (RANDOM() * (NOW() + '90 days' - NOW())))FROM generate_series(1, 10000000) s(i);
 
