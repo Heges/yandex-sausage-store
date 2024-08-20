1d720d3ae63bf6dd3ad03249c8c417c02655d657
gaag_alexander <ikunkka92@gmail.com>
2024-08-20 16:35:09 +0300

    update

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,18 +1,22 @@
 #! /bin/bash
 set -xe
+cd /home/student/frontend-build
+sudo cp -rf frontend-build/sausage.conf /etc/nginx/conf.d/sausage.conf
+sudo rm -rf /home/student/frontend-build/sausage-store-front.tar.gz
 
-sudo cp -rf sausage.conf /etc/nginx/conf.d/sausage.conf
-sudo rm -rf /home/student/sausage-store-front.tar.gz
-cd /home/student
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-front.tar.gz ${NEXUS_REPO_URL}/repository/${NEXUS_REPO_FRONTEND_NAME}/${VERSION}/sausage-store-${VERSION}.tar.gz
-sudo rm -rf /home/student/frontend
-tar -zxf ./sausage-store-front.tar.gz ||true
-sudo chown -R www-data:www-data ./frontend
+sudo rm -rf /home/student/frontend-build/frontend
+tar -zxf ./frontend-build/sausage-store-front.tar.gz ||true
+sudo chown -R www-data:www-data ./frontend-build/frontend
 sudo mkdir -p /var/www-data/frontend
-sudo cp -rf ./frontend/* /var/www-data/frontend
+sudo cp -rf ./frontend-build/frontend/* /var/www-data/frontend
+sudo cp -rf ~/frontend-build/Dockerfile ~/frontend/frontend-build/Dockerfile
 
 sudo systemctl daemon-reload
 sudo systemctl enable nginx
 sudo systemctl restart nginx
 
-docker run --rm --name sausage-store-fronted -p 9999:8080 -v ~/frontend-build/Dockerfile:/caddy
+cd ~/frontend-build/fronted
+
+docker build -t sausage-store-fronted-image:${VERSION}
+docker run --restart=always -p 9999:8080 --name=sausage-store-fronted sausage-store-fronted-image:${VERSION}
