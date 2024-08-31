a3099eafb45f2abf533bf13f256a033a01e1509f
gaag_alexander <ikunkka92@gmail.com>
2024-08-31 21:59:53 +0300

    update

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -12,10 +12,7 @@ sudo docker login -u "${CI_REGISTRY_USER}" -p "${CI_REGISTRY_PASSWORD}" "${CI_RE
 sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-backend || true
 sudo docker run --rm -d --name sausage-backend \
-     --env SPRING_DATASOURCE_URL="${SPRING_DATASOURCE_URL}" \
-     --env SPRING_DATASOURCE_USERNAME="${SPRING_DATASOURCE_USERNAME}" \
-     --env SPRING_DATASOURCE_PASSWORD="${SPRING_DATASOURCE_PASSWORD}" \
-     --env SPRING_DATA_MONGODB_URI="${SPRING_DATA_MONGODB_URI}" \
+     --env-file .env \
      --network=sausage_network \
      "${CI_REGISTRY_IMAGE}"/sausage-backend:"${VERSION}"
 
