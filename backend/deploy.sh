13e8ec111e691bc3addb80bb678bfd05e66bc5a8
gaag_alexander <ikunkka92@gmail.com>
2024-08-31 22:05:01 +0300

    update

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -11,7 +11,7 @@ EOL
 sudo docker login -u "${CI_REGISTRY_USER}" -p "${CI_REGISTRY_PASSWORD}" "${CI_REGISTRY}"
 sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-backend || true
-sudo docker run --rm -d --name sausage-backend \
+sudo docker run -d --name sausage-backend \
      --env-file .env \
      --network=sausage_network \
      "${CI_REGISTRY_IMAGE}"/sausage-backend:"${VERSION}"
