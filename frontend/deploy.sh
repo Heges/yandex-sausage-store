36dc02c6ca815c0e1bb1bd898238a2c771143615
gaag_alexander <ikunkka92@gmail.com>
2024-09-01 13:55:38 +0300

    Update deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -2,7 +2,7 @@
 set -xe
 sudo docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker network create -d bridge sausage_network || true
-sudo docker rm -f sausage-backend || true
+sudo docker rm -f sausage-frontend || true
 sudo docker run --rm --name sausage-frontend \
      --network=sausage_network -p 80:80 sausage-frontend:${VERSION} \
      "${CI_REGISTRY_IMAGE}"/sausage-frontend:${VERSION}
