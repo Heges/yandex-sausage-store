eb1795a1afe4b68bf247e9f82fe569446e556611
gaag_alexander <ikunkka92@gmail.com>
2024-09-01 15:12:55 +0300

    ujpodate

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 set -xe
 sudo docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
-sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-frontend || true
 sudo docker run --rm --name sausage-frontend \
      --network=sausage_network -p 80:80 sausage-frontend:${VERSION} \
