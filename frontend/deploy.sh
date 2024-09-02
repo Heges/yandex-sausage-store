5207018972eeddb94210c9e847713b5c80c83e41
gaag_alexander <ikunkka92@gmail.com>
2024-09-02 20:42:45 +0300

    update

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -6,6 +6,5 @@ sudo docker rm -f sausage-frontend || true
 sudo docker run -d --name sausage-frontend \
 	 --restart=always  \
      -p 80:80 \
-	 -v "$(pwd)/default.conf:/etc/nginx/conf.d/default.conf" \
 	 --network=sausage_network \
      "${CI_REGISTRY_IMAGE}"/sausage-frontend:${VERSION}
