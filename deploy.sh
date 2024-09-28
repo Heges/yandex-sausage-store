ab1fefa13aac2d3471a615eab21d84b225f8a4ab
gaag_alexander <ikunkka92@gmail.com>
2024-09-28 14:34:33 +0300

    1

--- deploy.sh
+++ deploy.sh
@@ -49,7 +49,7 @@ for container in $green_containers; do
 done
 
 echo "Подготавливаемся к запуску"
-blue_containers=$(get_containers_by_pattern "blue")
+blue_containers=$(docker ps --format "{{.Names}}" | grep blue | tr -d ' ')
 if [ "$green_healthy" = true ]; then
   echo "Green_healty = true начинаем с синего"
   deploy_container $blue_containers
@@ -63,25 +63,28 @@ if [ "$green_healthy" = true ]; then
 		echo "Выставляем blue_healhty в true"
       break
     fi
-    echo 'Waiting for $blue_containers to become healthy...'
+    echo "Waiting for ${blue_containers} to become healthy..."
     sleep 10
   done
 
   echo "backend-blue is now healthy. Stopping backend-green..."
-  stop_container $green_containers
+  grep_container_green_name=$(docker ps --format "{{.Names}}" | grep green | tr -d ' ')
+  stop_container $grep_container_green_name
 else
   echo "No backend-green container is healthy. Deploying backend-green first..."
-  deploy_container $blue_containers
+  grep_container_blue_name="$(docker ps --format "{{.Names}}" | grep blue | tr -d ' ')"
+  deploy_container $grep_container_blue_name
   
   # Ждем, пока backend-green станет healthy
 
   while true; do
+	grep_container_green_name=$(docker ps --format "{{.Names}}" | grep green | tr -d ' ')
     green_status=$(check_container_status $grep_container_green_name)
 	 echo "Статус контейнера: $green_status"
     if [ "$green_status" = "healthy" ]; then
       break
     fi
-    echo 'Waiting for $grep_container_green_name to become healthy... '
+    echo "Waiting for ${grep_container_green_name} to become healthy..."
     sleep 10
   done
 
