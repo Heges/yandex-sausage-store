f43358d991a5d3de579f8129de780fa58606fa46
gaag_alexander <ikunkka92@gmail.com>
2024-09-28 14:59:57 +0300

    1

--- deploy.sh
+++ deploy.sh
@@ -29,9 +29,14 @@ get_containers_by_pattern() {
   docker --context remote ps --format "{{.Names}}" | grep "$pattern" | tr -d ' ' 
 }
 
+get_container_by_name() {
+  local pattern=$1
+  docker --context remote ps --format "{{.Names}}" | grep blue -A 1 || echo "empty" | tr -d ' '
+}
+
 # Проверка состояния контейнеров backend-green
 
-green_containers=$(get_containers_by_pattern "backend-green")
+green_containers=$(get_containers_by_pattern "green")
 green_healthy=false
 
 for container in $green_containers; do
@@ -46,15 +51,15 @@ echo $green_healthy
 
 if [ "$green_healthy" = true ]; then
   echo "At least one backend-green container is healthy. Deploying backend-blue..."
-  bb=$(docker --context remote ps --format "{{.Names}}" | grep blue -A 1 || echo "empty" | tr -d ' ')
-  echo "deploy blue ${(docker --context remote ps --format "{{.Names}}" | grep blue -A 1 || echo "empty" | tr -d ' ')} 1" 
+  bb=$(get_container_by_name "blue")
+  echo "deploy blue $bb 1" 
   echo "deploy blue ${bb} 2" 
-  deploy_container $(docker --context remote ps --format "{{.Names}}" | grep blue -A 1 || echo "empty" | tr -d ' ') 
+  deploy_container $bb
 
   # Ждем, пока backend-blue станет healthy
 
   while true; do
-	bb=$(docker --context remote ps --format "{{.Names}}" | grep blue -A 1 || echo "empty" | tr -d ' ')
+	bb=$(get_container_by_name "blue")
     blue_status=$bb
     if [ "$blue_status" = "healthy" ]; then
       break
@@ -64,17 +69,17 @@ if [ "$green_healthy" = true ]; then
   done
 
   echo "backend-blue is now healthy. Stopping backend-green..."
-  bg=$(docker --context remote ps --format "{{.Names}}" | grep green -A 1 || echo "empty" | tr -d ' ')
+  bg=$(get_container_by_name "green")
   stop_container $bg
 else
   echo "No backend-green container is healthy. Deploying backend-green first..."
-  bg=$(docker --context remote ps --format "{{.Names}}" | grep green -A 1 || echo "empty" | tr -d ' ')
+  bg=$(get_container_by_name "green")
   deploy_container $bg
   
   # Ждем, пока backend-green станет healthy
 
   while true; do
-    green_status=$(check_container_status "backend-green")
+    green_status=$(check_container_status "green")
     if [ "$green_status" = "healthy" ]; then
       break
     fi
@@ -83,6 +88,6 @@ else
   done
 
   echo "backend-green is now healthy. Stopping backend-blue..."
-  bb=$(docker --context remote ps --format "{{.Names}}" | grep blue -A 1 || echo "empty" | tr -d ' ')
+  bb=$(get_container_by_name "blue")
   stop_container $bb
 fi
